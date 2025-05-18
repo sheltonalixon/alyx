f0df6089089e92e5d043036f3c974dcae3595a1b
Sheltón Alixón <sheltonlxn@gmail.com>
2025-05-18 13:47:29 -0400

    script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.getElementById('navToggle');
+    const navMenu = document.getElementById('navMenu');
+
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
+});
